258a34995f8ef05a4a18add97bbddf1cc3d12ec5
Calvin Lee <stalk.calvin@gmail.com>
2017-06-11 20:41:04 -0700

    CL: Add new kotlin algorithm: anagram counter

---
+++ src/main/kotlin/com/calvin/Arrays/AnagramCounter.kt
@@ -0,0 +1,17 @@
+package com.calvin.Arrays
+
+/*
+ * Copyright Calvin Lee Since 2016.
+ * All Rights Reserved.
+ */
+
+class AnagramCounter {
+    fun numberNeeded(first: String, second: String): Int {
+        var count = 0
+        val freq = IntArray(26)
+        for (c in first.toCharArray()) freq[c - 'a']++
+        for (c in second.toCharArray()) freq[c - 'a']--
+        for (i in freq) count += Math.abs(i)
+        return count
+    }
+}
